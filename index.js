8843da4cd2a25be87a54978518695ec42702509e
noodlehead55555 <108846638+noodlehead55555@users.noreply.github.com>
2026-07-14 19:25:11 -0600

    emergency download button fix also green comments

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
+To Do:
+- FIX THE DOWNLOAD
+- add file name changing
+- more colors
+- Add option to use preset YAML from another folder without needing to upload a file
+*/
+
+
 const input = document.querySelector("input")
 const output = document.getElementById("output")
 const fakeOutput = document.getElementById("fakeOutput")
@@ -26,7 +35,7 @@ function downloadClick() {
     if (!file) {
         return;
     }
-    downloadFile(output.textContent, file.name)
+    downloadFile(output.innerText, file.name)
 }
 
 function handleFileInput() {
@@ -43,7 +52,7 @@ function handleFileInput() {
     const reader = new FileReader()
     reader.onload = () => {
         output.innerText = reader.result
-        fakeOutput.innerText = output.innerText
+        updateFakeOutput()
     }
     reader.onerror = () => {
         console.log("there was some error i guess")
@@ -52,6 +61,32 @@ function handleFileInput() {
 }
 
 output.addEventListener("input",  (event) => {
-    fakeOutput.innerText = output.innerText
+    updateFakeOutput()
 })
-//below is exclusively for testing
+
+//A function that takes the innerHTML of output and adds spans for color (unfinished)
+function updateFakeOutput() {
+    var oldHTML = output.innerHTML
+    var workHTML = ""
+    var newHTML = output.innerHTML
+    var status = {//Keep track of what tags we've opened that we have to close
+        comment: false,
+        category: false,
+        options: false
+    }
+    var i
+    for (i=0;i<oldHTML.length;i++) {
+        if (oldHTML[i] === "#" && !status.comment) {
+            workHTML += '<span class="comment">#'
+            status.comment = true
+        } else if (oldHTML.slice(i, i+4) === "<br>" && status.comment) {
+            workHTML += '</span><'
+            status.comment = false
+        } else {
+            workHTML += oldHTML[i]
+        }
+    }
+    console.log(workHTML)
+    //newHTML += '<span class="comment">  #this is will show up in only the fakeOutput as GREEN</span>'
+    fakeOutput.innerHTML = workHTML
+}
